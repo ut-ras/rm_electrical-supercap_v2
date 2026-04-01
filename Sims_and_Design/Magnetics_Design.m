36cde6be4f2f3252902df663cc1c463f44dd842e
BaRho80 <eganwang2005@gmail.com>
2026-04-01 15:08:18 -0500

    inductor design file

--- Sims_and_Design/Magnetics_Design.m
+++ Sims_and_Design/Magnetics_Design.m
@@ -1,6 +1,9 @@
 clear all
 clc
-%%
+%% Part 1: Frequency Selection
+% We must first calculate our optimum switching frequency before optimizing
+% magnetics design. We make the assumption that at optimal frequency,
+% switching and conduction loss are equal. 
 R_on = 0.36e-3;
 I = 30;
 RR = 0.2;
@@ -12,19 +15,22 @@ v_l = 26;
 f_sw = 120e3;
 sw_loss = @(f_s) 2 * Coss * v_l^2 * f_s;
 sw_loss(f_sw)
-%% 
-%
-d_max = 9/16;
-L = d_max * v_l/ (f_sw * RR * I)
-I_max = (1+RR) * I;
-B_max = 390/1000;
+%% Part 2: Inductor Design
+% We now design our inductor. We are using an ETD44 core with N97 ferrite
+d_max = 9/16; % Maximum duty ratio
+L = d_max * v_l/ (f_sw * RR * I) % Target inductance
+I_max = (1+RR) * I; % Peak current
+B_max = 390/1000; % Saturation B-field
 safety_margin = 1.2;
-A_c = 76e-6;
-N = 8;
+A_c = 173e-6; % Core area
+N_sat = L * I_max / (B_max * A_c) % # of turns to avoid saturation
+%%
+% 
+N = ceil(N_sat);
 total_reluctance = N * I_max / (A_c * B_max * safety_margin)
 l_c = 0.074;
 mu_0 = 4 * pi * 1e-7;
-mu_r = 1650;
+mu_r = 2300;
 core_reluctance = l_c / (mu_0 * mu_r * A_c);
 gap_reluctance = total_reluctance - core_reluctance
 gap_length = mu_0 * A_c * gap_reluctance;
