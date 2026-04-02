09248d815d6c806dac1920aad6be5699ba0e9b30
BaRho80 <eganwang2005@gmail.com>
2026-04-01 19:33:44 -0500

    updated PLECs model to use new inductor parameters

--- Sims_and_Design/Magnetics_Design.m
+++ Sims_and_Design/Magnetics_Design.m
@@ -16,7 +16,10 @@ f_sw = 120e3;
 sw_loss = @(f_s) 2 * Coss * v_l^2 * f_s;
 sw_loss(f_sw)
 %% Part 2: Inductor Design
-% We now design our inductor. We are using an ETD44 core with N97 ferrite
+% We now design our inductor. We are using an ETD44 core with N97 ferrite.
+% Unlike typical applications which may seek to choose a core size to
+% optimize physical size, we wish to specifically optimize ESR at DC, which
+% means picking the largest core available (ETD44). 
 d_max = 9/16; % Maximum duty ratio
 L = d_max * v_l/ (f_sw * RR * I) % Target inductance
 I_max = (1+RR) * I; % Peak current
@@ -25,7 +28,8 @@ safety_margin = 1.2;
 A_c = 173e-6; % Core area
 N_sat = L * I_max / (B_max * A_c) % # of turns to avoid saturation
 %%
-% 
+% We thus choose 11 turns. We now determine the air gap size to achieve the
+% desired saturation and inductance characteristics. 
 N = ceil(N_sat);
 total_reluctance = N * I_max / (A_c * B_max * safety_margin)
 l_c = 0.074;
@@ -34,4 +38,4 @@ mu_r = 2300;
 core_reluctance = l_c / (mu_0 * mu_r * A_c);
 gap_reluctance = total_reluctance - core_reluctance
 gap_length = mu_0 * A_c * gap_reluctance;
-real_gap_length = 0.5 * gap_length
+disp(strcat("Gap size: ",string(gap_length * 1000)," mm"));
